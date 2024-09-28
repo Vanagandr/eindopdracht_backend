a874ce626eb601b4b5efd129682d70e755942a50
Vanagandr <lukeforster@live.nl>
2024-09-28 16:44:40 +0200

    Toevoeging dependencies

---
+++ src/main/java/com/eindopdracht/eindopdracht_forster/EindopdrachtForsterApplication.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.eindopdracht_forster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtForsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtForsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eindopdracht/eindopdracht_forster/EindopdrachtForsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.eindopdracht_forster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtForsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
