1c74da530fd2b528debbc0dc5d26e6accedb9898
Vanagandr <lukeforster@live.nl>
2024-10-01 14:46:25 +0200

    sommige data in data.sql geladen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,23 @@
 insert into roles(rolename) values ('ROLE_ADMINISTRATION'), ('ROLE_MECHANIC'), ('ROLE_SUPPLY');
+
+insert into parts (type, price, quantity) values ('Band', 100, 20);
+insert into parts (type, price, quantity) values ('Accu', 120, 15);
+insert into parts (type, price, quantity) values ('Motorolie', 40, 50);
+insert into parts (type, price, quantity) values ('Remblokken', 70, 30);
+insert into parts (type, price, quantity) values ('Dynamo', 200, 10);
+insert into parts (type, price, quantity) values ('Waterpomp', 90, 25);
+insert into parts (type, price, quantity) values ('Brandstoffilter', 30, 40);
+insert into parts (type, price, quantity) values ('Luchtfilter', 20, 35);
+insert into parts (type, price, quantity) values ('Koplampen', 150, 18);
+insert into parts (type, price, quantity) values ('Radiateur', 180, 12);
+
+insert into repairs (type, price) values ('Band Vervangen', 100);
+insert into repairs (type, price) values ('Olie Verversen', 50);
+insert into repairs (type, price) values ('Accu Vervangen', 200);
+insert into repairs (type, price) values ('Remblokken Vervangen', 70);
+insert into repairs (type, price) values ('Dynamo Vervangen', 250);
+insert into repairs (type, price) values ('Waterpomp Vervangen', 150);
+insert into repairs (type, price) values ('Brandstoffilter Vervangen', 40);
+insert into repairs (type, price) values ('Luchtfilter Vervangen', 30);
+insert into repairs (type, price) values ('Koplampen Vervangen', 200);
+insert into repairs (type, price) values ('Radiateur Vervangen', 180);
